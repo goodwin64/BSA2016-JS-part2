e84f5ca1bbcf6d5db3e0d67991d062c6061d3d0d
Max Donchenko <wereya2@yandex.ru>
2016-07-15 00:23:51 +0300

    Task 4: standard syntax replaced by ES6

--- BSA 16 JS - ECMA Script 2015/index.js
+++ BSA 16 JS - ECMA Script 2015/index.js
@@ -1,6 +1,7 @@
 ;
 
 class Fighter {
+	/* Default settings */
 	constructor(name, power, health) {
 		this.name = name || "Unnamed fighter";
 		this.power = power || 10;
@@ -19,15 +20,22 @@ class Fighter {
 		let damage = point * this.power;
 		enemy.damage = damage;
 	}
+
+	/* String interpolation */
+	toString() {
+		return this.constructor.name + ` ${this.name}, power: ${this.power}, health: ${this.health}`;
+	}
 }
 
+/* Inheritance */
 class ImprovedFighter extends Fighter {
 	doubleHit(enemy, point) {
 		super.hit(enemy, point * 2);
 	}
 }
 
-function fight(fighter, improvedFighter, ...points) {
+/* Arrow function + rest */
+var fight = (fighter, improvedFighter, ...points) => {
 	let firstIsLoser = true;
 
 	for (let i = 0; i < points.length; i++) {
@@ -41,11 +49,10 @@ function fight(fighter, improvedFighter, ...points) {
 		}
 	}
 
-	console.log("The winner is");
-	console.log(firstIsLoser ? improvedFighter : fighter);
+	console.log("The winner is " + (firstIsLoser ? improvedFighter : fighter));
 }
 
 var f1 = new Fighter("Alex", 12, 500);
 let f2 = new ImprovedFighter("Max", 16, 800);
 
-fight(f1, f2, 25, 13, 45);
+fight(f1, f2, 25, 13, 45); // The winner is ImprovedFighter Max, power: 16, health: 500
