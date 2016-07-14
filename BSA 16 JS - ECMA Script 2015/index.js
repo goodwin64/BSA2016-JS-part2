e71ce957c7b2b3d1b2ff073278f10973a08f3cd6
Max Donchenko <wereya2@yandex.ru>
2016-07-14 23:50:15 +0300

    Task 4: add <ImprovedFighter> class

--- BSA 16 JS - ECMA Script 2015/index.js
+++ BSA 16 JS - ECMA Script 2015/index.js
@@ -21,10 +21,18 @@ class Fighter {
 	}
 }
 
-var f1 = new Fighter("Alex", 12, 500);
-if (f1.power > 100) {
-	let f2 = new Fighter();
+class ImprovedFighter extends Fighter {
+	doubleHit(enemy, point) {
+		super.hit(enemy, point * 2);
+	}
 }
 
-console.log(f1); // Fighter {name: "Alex", power: 12, health: 500}
-console.log(f2); // ReferenceError: f2 is not defined
+var f1 = new Fighter("Alex", 12, 500);
+let f2 = new ImprovedFighter("Max", 16, 800);
+
+console.log(f1); 			// Fighter {name: "Alex", power: 12, health: 500}
+console.log(f2);	// ImprovedFighter {name: "Max", power: 16, health: 800}
+console.log(f1 instanceof Fighter); 		// true
+console.log(f1 instanceof ImprovedFighter); // false
+console.log(f2 instanceof Fighter); 		// true
+console.log(f2 instanceof ImprovedFighter); // true
